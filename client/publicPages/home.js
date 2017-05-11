deac0ca018aa28c96b51bceef6ac1af164663ce8
Tonny <oursonvie@qq.com>
2017-05-11 11:23:45 +0800

    homepage news panel

--- client/publicPages/home.js
+++ client/publicPages/home.js
@@ -2,11 +2,21 @@ Template.home.onCreated(function(){
    var self = this;
     self.autorun(function() {
        self.subscribe('NewsAll');
+       self.subscribe('ProgramsAll');
     });
 });
 
 Template.home.helpers({
   news: function() {
-    return News.findOne()
+    return News.find({});
+  },
+  clean: function(content) {
+    return content.replace(/<(?:.|\n)*?>/gm, '').substring(0,50) + ' ...';
+  },
+  newImg: function() {
+    var myRegex = /<img[^>]+src="(http:\/\/[^">]+)"/g;
+    var htmlContent = News.findOne({_id:this._id}).content
+    var img = myRegex.exec(htmlContent)[0]
+    return img+' class="img-thumbnail">';
   }
 })
