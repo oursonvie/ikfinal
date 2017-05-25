b2e1b8c4868061fd141bbd20f99af3aa86168358
Tonny <oursonvie@qq.com>
2017-05-25 10:17:16 +0800

    minor backup

--- server/methods/downloadStudentTable.js
+++ server/methods/downloadStudentTable.js
@@ -1,6 +1,10 @@
 Meteor.methods({
   // process csv file for download
   download: function(programId) {
-    return CSV.unparse(Students.find({'enrollment.programId':programId},{fields:{'_id':0, 'userId':0, 'enrollment':0, 'profile_image':0}}).fetch());
+    return CSV.unparse(Students.find({'enrollment.programId':programId}).fetch())
+
+    /*
+    return CSV.unparse(Students.find({'enrollment.programId':programId},{fields:{'userId':0, 'enrollment':0, 'profile_image':0}}).fetch());
+    */
   }
 });
