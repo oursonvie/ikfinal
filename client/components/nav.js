55b77b4b9b98ef035079c54428d432b98601147b
Tonny <oursonvie@qq.com>
2017-07-25 10:13:14 +0800

    public pages tanslated

---
+++ client/components/nav.js
@@ -0,0 +1,28 @@
+Template.nav.helpers({
+
+  languages() {
+    const obj = TAPi18n.getLanguages();
+    const languages = [];
+    for (const key in obj) {
+      if (key) languages.push({ code: key, labels: obj[key] });
+    }
+    console.log(languages)
+    if (languages) return languages;
+  },
+
+  currentLanguage() {
+    const currentLanguageCode = TAPi18n.getLanguage();
+    const appLanguages = TAPi18n.getLanguages();
+    for (const code in appLanguages) {
+      if (code === currentLanguageCode) return appLanguages[code].name;
+    }
+  }
+});
+
+Template.nav.events({
+  'click .changeLang' () {
+    console.log(this.code)
+    TAPi18n.setLanguage(this.code);
+  }
+
+});
